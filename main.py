800b3b93f1174e2036b9d29ee90ef94d9b43c967
Alisson Costa Schmidt <84454233+Atomiqui@users.noreply.github.com>
2023-09-11 20:13:31 +0000

    att

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-#Grupo:
-#Alisson, Ana, Francisco e Enzo.
+# Grupo:
+# Alisson, Ana, Francisco e Enzo.
 
 import sys
 from Quintuple import Quintuple
 from ReversibleTuringMachine import ReversibleTuringMachine
 
+# Para rodar no windows:
+
+# Comente as próximas duas linhas e
 file = sys.stdin
 content = file.readlines()
 
+# descomente as próximas duas linhas.
 #with open('entrada-quintupla.txt', 'r') as file:
 #    content = file.readlines()
     
@@ -22,6 +26,15 @@ for i in range(4, 4 + int(num_transitions)):
     transitions.append(Quintuple(content[i]))
 
 entry = content[-1]
+
+print("MT Lida:\n")
+print(num_states + " " + num_input_symbols + " " + num_queue_symbols + " " + num_transitions)
+print(' '.join(str_states))
+print(' '.join(input_symbols))
+print(' '.join(queue_symbols))
+#print(' '.join(str_states))
+for transition in transitions:
+    print(transition)
     
 reversibleTuringMachine = ReversibleTuringMachine(num_states, input_symbols, queue_symbols, transitions, entry)
 
